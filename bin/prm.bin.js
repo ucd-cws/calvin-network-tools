16cf87ced0a5e5b8871e75a196d62b07e0cd410b
Justin Merz <jrmerz@gmail.com>
2016-01-15 13:07:01 -0800

    adding global install

---
+++ bin/prm.bin.js
@@ -0,0 +1,3 @@
+#! /usr/bin/env node
+
+require('../nodejs/prm');
--- nodejs/prm/index.js
+++ nodejs/prm/index.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 var path = require('path');
 var argv = require('minimist')(process.argv.slice(2));
 var crawler = require('../crawler');
-
+var docsUrl = 'https://github.com/ucd-cws/calvin-network-tools/blob/master/README.md#commands';
 
 var noCommand = false;
 if( !argv._ ) {
@@ -12,12 +12,21 @@ if( !argv._ ) {
 } else if ( argv._.length === 0 ) {
   noCommand = true;
 }
+
 if( noCommand ) {
-  return console.log('Please provide a command.');
+  return console.log('Please provide a command. See docs here: '+docsUrl);
+}
+
+
+var cmd = argv._.splice(0, 1)[0];
+
+// check for init command.
+if( cmd === 'init' ) {
+  require('../../init.js');
+  return;
 }
 
 // check for link, we are using node module for link commands
-var cmd = argv._.splice(0, 1)[0];
 if( cmd === 'show' ) {
   cmd = 'node';
   argv.show = true;
@@ -41,7 +50,7 @@ if( cmd === 'pd' || cmd === 'ts' || cmd === 'el' ) {
   if( cmd === 'show' ) {
     return require('./cmds/showBuild')(type, argv);
   } else {
-    console.log('Invalid command for '+type+': '+cmd);
+    console.log('Invalid command for '+type+': '+cmd+'. See docs here '+docsUrl);
     process.exit(-1);
   }
 }
@@ -49,7 +58,7 @@ if( cmd === 'pd' || cmd === 'ts' || cmd === 'el' ) {
 var modulePath = path.join(__dirname, 'cmds', cmd+'.js');
 
 if( !fs.existsSync(modulePath) ) {
-  return console.log('Invalid command: '+cmd);
+  return console.log('Invalid command: '+cmd+'. See docs here '+docsUrl);
 }
 
 require(modulePath)(argv);
