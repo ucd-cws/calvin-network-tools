30f93acb113f88a25af3cd226757dfb963dc04ab
Justin Merz <jrmerz@gmail.com>
2016-04-18 10:23:38 -0700

    little cleanup

--- nodejs/matrix/index.js
+++ nodejs/matrix/index.js
@@ -5,35 +5,34 @@
 // j= terminal
 // k = link number
 // c = cost
-// a =amplitude
+// a = amplitude
 // l = lower bound
 // u = upper bound
 
 
 'use strict';
 
-var hnf=require('hnf');
-var split=require('./split');
+var hnf = require('hobbes-network-format');
+var split = require('./split');
 
 module.exports = function matrix() {
-    
-    var sets=split(config.path,ins);
-      // Need to consider what updateStorge does with times
-      for( var i = 0; i < list.length; i++ ) {
-	  var net=list[i].properties;
-	  if ((config.matrixB || true) && net.inflows) {
-	      for (var key in net.inflows) {
-		  var fn=net.inflows[key].inflow;
-		  if (fn.match(/.*\.csv$/)) {
-		      parse_csv(fn,function(err,data) {
-			  data.forEach(function(d) {
-			      console.log([[net.prmname,d[0]].join('@'),d[1]].join(','));
-			  });
-		      });
-		  }
+
+    var sets = split(config.path, ins);
+    // Need to consider what updateStorge does with times
+    for( var i = 0; i < list.length; i++ ) {
+      var net = list[i].properties;
+
+      if( (config.matrixB || true) && net.inflows) {
+        for (var key in net.inflows) {
+          var fn = net.inflows[key].inflow;
+          if( fn.match(/.*\.csv$/) ) {
+            parse_csv(fn,function(err,data) {
+              data.forEach(function(d) {
+                console.log([[net.prmname,d[0]].join('@'),d[1]].join(','));
+              });
+            });
+          }
+        }
       }
-	  }
-      }
-      callback();
-  });
-}
+    }
+};
--- nodejs/matrix/split.js
+++ nodejs/matrix/split.js
@@ -1,3 +1,5 @@
+'use strict';
+
 /*
  # split -
 
@@ -8,15 +10,15 @@
  edge - All Links and Nodes on the edge of the network.
  options:
  linked : [boolean] } : Default is true.
- expand : {inflows:1} : Standard expansion option 
- 
+ expand : {inflows:1} : Standard expansion option
+
 ## OPTIONS
 
 ### Linked
 
 If linked is true, here are the rules.
 - Any link where both the origin and terminal nodes are *in* is *in*.
-- Any link where one of origin / terminal are in is *edge*. 
+- Any link where one of origin / terminal are in is *edge*.
 - Any link where neither origin / terminal are in is *out*.
 - Any node with a link *in* and a link *out* is *edge*
 - Any node with all links *in* is *in*
@@ -24,70 +26,76 @@ If linked is true, here are the rules.
 
 */
 
-require(hnf);
+var hnf = require('hobbes-network-format');
 
-module.exports = function(path,opts,item_list,callback) {
-    var linked=opts.linked || true;
-    delete(opts.linked);
-    var ins=[];
-    var outs=[];
-    var edges=[];
+module.exports = function(path, opts, item_list, callback) {
+    var linked = opts.linked || true;
+    delete opts.linked ;
+    var ins = [];
+    var outs = [];
+    var edges = [];
 
-    var items={};
+    var items = {};
+
     // This could also check for features and use id if that's what they are
-    item_list.forEach(function(i) { items[i]++; }
-		      
-    
-    hnf.crawl
-    (path,opts,
-     function(result) {
-	 var all=results.nodes.features;
-	 var one_in={};
-	 var one_out={};
-	 var maybe_in={};
-	 all.forEach(n) {
-	     var p = n.properties;
-	     if (p.origin && p.terminal) { // Link
-		 if (items[p.id]) {                    // Explicitly *in*
-		     ins.push(n);
-		     one_in[n.origin]++;
-		     one_in[n.terminal]++;
-		 } else {
-		     if (nodes[n.origin]) {
-			 if (nodes[n.terminal]) {
-			     ins.push(n);
-			     one_in[n.origin]++;
-			     one_in[n.terminal]++;
-			 } else {
-			     edges.push(n);
-			 }
-		     } else {
-			 if (nodes[n.terminal]) {
-			     edges.push(n);
-			 } else {
-			     outs.push(n);
-			     one_in[n.origin]++;
-			     one_in[n.terminal]++;
-			 }
-		     }
-		 }
-	     } else {		// Node
-		 if (items[p.id]) {                    // Explicitly *in*
-		     ins.push(n);
-		 } else {
-		     maybe_in[p.id]=n;
-		 }
-	     }
-	 }
-	 // Now check for any nodes that are in by virtual of all links are *in*
-	 for (var maybe in one_in) {
-	     if (! one_out[maybe] && maybe_in[maybe]) {
-		 ins.push(maybe_in[maybe]);
-	     }
-	 }
-	 // Okay return our sets
-	 callback({in:ins,edge:edges,out:outs});
-     });
-    
-    
+    item_list.forEach(function(i) {
+      items[i] = true;
+    });
 
+
+    hnf.crawl(path, opts, function(result) {
+      var all = result.nodes.features;
+      var one_in = {};
+      var one_out = {};
+      var maybe_in = {};
+
+      all.forEach(function(n){
+        var p = n.properties;
+        if( p.origin && p.terminal ) { // Link
+          if( items[p.id] ) {         // Explicitly *in*
+            ins.push(n);
+            one_in[n.origin] = true;
+            one_in[n.terminal] = true;
+          } else {
+            if (items[n.origin]) {
+              if (items[n.terminal]) {
+                ins.push(n);
+                one_in[n.origin] = true;
+                one_in[n.terminal] = true;
+              } else {
+                edges.push(n);
+              }
+            } else {
+              if (items[n.terminal]) {
+                edges.push(n);
+              } else {
+                outs.push(n);
+                one_in[n.origin] = true;
+                one_in[n.terminal] = true;
+              }
+            }
+          }
+        } else {    // Node
+          if (items[p.id]) {                    // Explicitly *in*
+            ins.push(n);
+          } else {
+            maybe_in[p.id]=n;
+          }
+       }
+     });
+
+     // Now check for any nodes that are in by virtual of all links are *in*
+     for( var maybe in one_in ) {
+       if( !one_out[maybe] && maybe_in[maybe]) {
+         ins.push(maybe_in[maybe]);
+       }
+     }
+
+     // Okay return our sets
+     callback({
+       in : ins,
+       edge : edges,
+       out : outs
+     });
+  });
+};
