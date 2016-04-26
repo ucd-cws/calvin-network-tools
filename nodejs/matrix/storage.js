b78c0186abb08dd6695d9e606679c37cd28761e9
Quinn Hart <qjhart@ucdavis.edu>
2016-04-26 11:54:11 -0700

    Started a replacement of prm with cnf. Added cnf-matrix

--- nodejs/matrix/storage.js
+++ nodejs/matrix/storage.js
@@ -36,62 +36,62 @@ module.exports = function(stor, steps) {
       if (! first) {
         if (i>1) initial=cap[i-1][1];
         first++;
+      }
+      last=i;
+    }
+  }
+  if (last !==cap.length-1) {
+    ending=cap[last+1][1];
+  }
+
+  // Add Initial [i,j,k,cost,amplitude,lower,upper]
+  rows.push(['INITIAL',u.id(id,steps[0]),0,0,1,initial,initial]);
+
+  var step_costs = cost(p.costs, steps);
+  var step_bounds = bound(p.bounds, steps);
+  var step_amp = evaporation(stor, steps);
+  var i;
+  var lb,ub,costs;
+  var clb,cub;
+  var amp;
+  var next;
+
+  for(i = 0; i < steps.length; i++ ) { // i=0 is header;
+    lb = step_bounds[i][0];
+    ub = step_bounds[i][1];
+    costs = step_costs[i];
+    amp=step_amp[i];
+
+    if(i===steps.length-1) { // Fixed to final storage
+      lb = ending;
+      ub = ending;
+      next='FINAL';
+      rows.push([u.id(id,steps[i]),next,0,0,1,lb,ub]);
+    } else {
+      next=u.id(id,steps[i+1]);
+      for( c = 0; c < costs.length; c++ ){
+        clb=( costs[c][1] > lb )
+        ? costs[c][1]
+        : ( (costs[c][2] || 0) <= lb )
+        ? (costs[c][2] || 0)
+        : lb;
+
+        lb -= clb;
+        if (ub===null) {
+          cub=costs[c][2];
+        } else {
+          cub = ( costs[c][2]!==null && costs[c][2] <= ub ) ? costs[c][2] : ub;
+          ub -= cub;
         }
-        last=i;
-        }
-        }
-        if (last !==cap.length-1) {
-          ending=cap[last+1][1];
-          }
 
-          // Add Initial [i,j,k,cost,amplitude,lower,upper]
-          rows.push(['INITIAL',u.id(id,steps[0]),0,0,1,initial,initial]);
+        if (cub===null || cub>0) {
+          rows.push([u.id(id,steps[i]),next,
+          c, costs[c][0], amp, clb, cub
+        ]);
+      }
+    }
+  }
+}
 
-          var step_costs = cost(p.costs, steps);
-          var step_bounds = bound(p.bounds, steps);
-          var step_amp = evaporation(stor, steps);
-          var i;
-          var lb,ub,costs;
-          var clb,cub;
-          var amp;
-          var next;
-
-          for(i = 0; i < steps.length; i++ ) { // i=0 is header;
-            lb = step_bounds[i][0];
-            ub = step_bounds[i][1];
-            costs = step_costs[i];
-            amp=step_amp[i];
-
-            if(i===steps.length-1) { // Fixed to final storage
-              lb = ending;
-              ub = ending;
-              next='FINAL';
-              } else {
-                next=u.id(id,steps[i+1]);
-                }
-
-                for( c = 0; c < costs.length; c++ ){
-                  clb=( costs[c][1] > lb )
-                  ? costs[c][1]
-                  : ( (costs[c][2] || 0) <= lb )
-                  ? (costs[c][2] || 0)
-                  : lb;
-
-                  lb -= clb;
-                  if (ub===null) {
-                    cub=costs[c][2];
-                    } else {
-                      cub = ( costs[c][2]!==null && costs[c][2] <= ub ) ? costs[c][2] : ub;
-                      ub -= cub;
-                      }
-
-                      if (cub===null || cub>0) {
-                        rows.push([u.id(id,steps[i]),next,
-                        c, costs[c][0], amp, clb, cub
-                        ]);
-                        }
-                        }
-                        }
-
-                        return rows;
-                      };
+return rows;
+};
