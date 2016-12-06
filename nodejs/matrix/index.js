973f4c460a32cfbb0208f7b4c521abc1bdc8c3ff
Justin Merz <jrmerz@gmail.com>
2016-12-06 09:14:01 -0800

    removing disabled nodes, moving to hnf v2, preparing for v2 rollout

--- nodejs/matrix/index.js
+++ nodejs/matrix/index.js
@@ -49,6 +49,17 @@ function matrix(config, callback) {
       subnet.in = debug(subnet.in);
     }
 
+    // remove all disabled nodes
+    ['in', 'out'].forEach((type) => {
+      var l = subnet[type].length;
+      for( var i = l-1; i >= 0; i-- ) {
+        if( subnet[type][i].properties.disabled ) {
+          subnet[type].splice(i, 1);
+        }
+      }
+    });
+    
+
     expand(subnet, function(){
 
       // JM - same as above
